3249cb9f1ed42f988a3e89ea8a00fb8abcb9dbe2
ShiroYacha (Min Zhao) <sebzhaomin@gmail.com>
2016-01-17 21:46:41 +0100

    Add license info from frayxrulez

--- UWPToolkit/Controls/SwipeableItem.cs
+++ UWPToolkit/Controls/SwipeableItem.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// ----------------------------------------------------
+// Modified from FrayxRulez/SwipeListView (MIT License)
+// ----------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

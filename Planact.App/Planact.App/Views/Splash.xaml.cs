40cde798f9199acca6f3200c98233a74d22d8660
ShiroYacha (Min Zhao) <sebzhaomin@gmail.com>
2016-01-20 21:43:31 +0100

    Small UX fixes and redesign the layout of the timelinehub

--- Planact.App/Planact.App/Views/Splash.xaml.cs
+++ Planact.App/Planact.App/Views/Splash.xaml.cs
@@ -11,27 +11,6 @@ namespace Planact.App.Views
         public Splash(SplashScreen splashScreen)
         {
             this.InitializeComponent();
-
-            Action resize = () =>
-            {
-                if (splashScreen.ImageLocation.Top == 0)
-                {
-                    MyImage.Visibility = Visibility.Collapsed;
-                    return;
-                }
-                else
-                {
-                    MyCanvas.Background = null;
-                    MyImage.Visibility = Visibility.Visible;
-                }
-                MyImage.Height = splashScreen.ImageLocation.Height;
-                MyImage.Width = splashScreen.ImageLocation.Width;
-                MyImage.SetValue(Canvas.TopProperty, splashScreen.ImageLocation.Top);
-                MyImage.SetValue(Canvas.LeftProperty, splashScreen.ImageLocation.Left);
-                ProgressTransform.TranslateY = MyImage.Height / 2;
-            };
-            Window.Current.SizeChanged += (s, e) => resize();
-            resize();
         }
     }
 }

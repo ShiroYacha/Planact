ef9b3a0f98b34981dbeea2b6f74916bfaa546c9a
ShiroYacha (Min Zhao) <sebzhaomin@gmail.com>
2016-01-22 00:27:28 +0100

    Add new design time project

---
+++ Planact.App/Planact.DesignTime/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planact.DesignTime
+{
+    public class Class1
+    {
+    }
+}
